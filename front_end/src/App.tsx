b3abb9d24e35bb701ef27c62f572093e7f77d3e8
Kit Fuderich <kitfuderich@gmail.com>
2022-01-23 12:13:19 -0800

    metamask wallet connected/disconnect with button

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { DAppProvider, ChainId } from "@usedapp/core"
+import { Header } from "./components/Header"
 
 function App() {
   return (
     <DAppProvider config={{
       supportedChains: [ChainId.Kovan, ChainId.Rinkeby]
     }}>
+      <Header></Header>
       <div>
         Hello there!
       </div>
